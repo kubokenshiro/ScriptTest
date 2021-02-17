3c955ba812ab92649086b74eb9f1d55333885f79
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-02-17 11:19:58 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+    int[] array = {1, 2, 3, 4, 5};
+    // array.ForEachReverse( i => Debug.Log(i));
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Debug.Log(array[i]);
+    }
+        
+    }
+   
+}
