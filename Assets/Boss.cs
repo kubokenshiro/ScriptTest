ea39e8dfef60ca686d3d9381e7655e03806c1e75
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-02-17 11:44:03 +0900

    Bossクラス追加
    課題Bossクラス追加しました。

---
+++ Assets/Boss.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Magic(int magic)
+    {
+        this.mp -= magic;
+        if(mp >= 5)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else if(mp <= 0)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,14 +6,12 @@ public class Test : MonoBehaviour
 {
     void Start()
     {
-    int[] array = {1, 2, 3, 4, 5};
-    // array.ForEachReverse( i => Debug.Log(i));
-
-    for(int i = 0; i < array.Length; i++)
-    {
-        Debug.Log(array[i]);
+        Boss lastboss = new Boss();
+        lastboss.Attack();
+        lastboss.Defence(3);
+        lastboss.Magic(5);
     }
         
-    }
+    
    
 }
